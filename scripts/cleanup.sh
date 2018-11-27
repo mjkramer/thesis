b6840b8a9135cafc4c52db7214683cac8e375ed8
Matt Kramer <matt314159@gmail.com>
2018-11-27 15:57:31 -0800

    Enhance cleanup script

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+cd $(dirname $0)/..
+
+while getopts "p" o; do
+    case "$o" in
+        p) rmpdf=1;;
+    esac
+done
+shift $((OPTIND-1))
+
 exts="aux bbl bcf blg dvi fdb_latexmk fls lof log lot out run.xml synctex.gz toc"
 
-find -regex '.*\.\('$(echo $exts | sed 's/ /\\|/g')'\)' | xargs rm
+if [[ $rmpdf ]]; then
+    exts="$exts pdf"
+fi
+
+# don't delete pdfs from ./refs or ./images!
+spare_dirs='( -path ./refs -o -path ./images ) -prune'
+
+find $spare_dirs -o -regex '.*\.\('$(echo $exts | sed 's/ /\\|/g')'\)' -print | xargs -t rm
