96e6abc2be05064f6cb97a81ce28cb0245faacc8
Matt Kramer <matt314159@gmail.com>
2020-05-22 03:41:36 -0700

    Extract macros from Introduction.tex

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -9,7 +9,7 @@ while getopts "p" o; do
 done
 shift $((OPTIND-1))
 
-exts="aux bbl bcf blg dvi fdb_latexmk fls lof log lot out run.xml synctex.gz toc"
+exts="aux bbl bcf blg dvi fdb_latexmk fls fmt lof log lot out run.xml synctex.gz toc"
 
 if [[ $rmpdf ]]; then
     exts="$exts pdf"
@@ -18,4 +18,6 @@ fi
 # don't delete pdfs from ./refs or ./images!
 spare_dirs='( -path ./refs -o -path ./images ) -prune'
 
-find $spare_dirs -o -regex '.*\.\('$(echo $exts | sed 's/ /\\|/g')'\)' -print | xargs -t rm
+find $spare_dirs -o -regex '.*\.\('$(echo $exts | sed 's/ /\\|/g')'\)' -print | xargs -t rm -f
+find $spare_dirs -o -regex '.*/_region_\.tex' -print | xargs -t rm -f
+find $spare_dirs -o -regex '.*/_region_\.prv' -print | xargs -t rm -rf
