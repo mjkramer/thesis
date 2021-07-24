e89af1f48a47e441cb51762cb0cfc61520e0846a
Matt Kramer <matt314159@gmail.com>
2021-07-24 16:50:37 -0700

    Finish up Li9

--- .dir-locals.el
+++ .dir-locals.el
@@ -2,6 +2,7 @@
 ;;; For more information see (info "(emacs) Directory Variables")
 
 ((latex-mode . ((bibtex-dialect . 'biblatex)
+                (LaTeX-default-tabular-environment . "tabular")
                 ;; (reftex-ref-style-default-list . '("Hyperref"))
                 ;; (reftex-ref-macro-prompt . nil)
                 )))
