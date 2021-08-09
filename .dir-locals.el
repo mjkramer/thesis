9e6d58c197b05e4644b4155ef8c976584929f9bb
Matt Kramer <matt314159@gmail.com>
2021-08-09 16:01:20 -0700

    Merge longlines branch
    Master branch is now where we work on final draft. Meanwhile, longlines branch
    will become nGd technote.

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,4 +1,8 @@
 ;;; Directory Local Variables
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((latex-mode . ((bibtex-dialect . 'biblatex))))
+((latex-mode . ((bibtex-dialect . 'biblatex)
+                (LaTeX-default-tabular-environment . "tabular")
+                ;; (reftex-ref-style-default-list . '("Hyperref"))
+                ;; (reftex-ref-macro-prompt . nil)
+                )))
