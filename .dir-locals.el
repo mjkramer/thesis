c50eca229bea8b20a6b809d64137497019304cf4
Matt Kramer <matt314159@gmail.com>
2021-12-10 09:57:46 -0800

    Update li9 linreg plots

--- .dir-locals.el
+++ .dir-locals.el
@@ -3,6 +3,8 @@
 
 ((latex-mode . ((bibtex-dialect . 'biblatex)
                 (LaTeX-default-tabular-environment . "tabular")
+                (reftex-default-bibliography . "/home/mkramer/physics/thesis/biblio.bib")
+                (org-cite-default-bibliography . "/home/mkramer/physics/thesis/biblio.bib")
                 ;; (reftex-ref-style-default-list . '("Hyperref"))
                 ;; (reftex-ref-macro-prompt . nil)
                 )))
